7b94877733e8400c0a3ecca79bca962f3ddae7d0
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-10 18:51:18 +0530

    Routes setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import HotelListPage from "./pages/HotelListPage";
+import SingleHotelPage from "./pages/SingleHotelPage";
 
 function App() {
   return (
     <>
-    <p>hii</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hotels" element={<HotelListPage />} />
+          <Route path="/hotels/:id" element={<SingleHotelPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
